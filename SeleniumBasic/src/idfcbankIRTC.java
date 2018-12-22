b4db50157fb950aece1521038a6069f1daa74eaa
AAdios <varma096@gmail.com>
2018-12-22 19:50:11 +0530

    new

---
+++ SeleniumBasic/src/idfcbankIRTC.java
@@ -0,0 +1,42 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class idfcbankIRTC {
+		WebDriver driver;
+	
+	
+	/*public void invokeBrowser()
+	{
+		try {
+			System.setProperty("webdriver.gecko.driver", "C:\\Users\\geckodriver.exe");
+			 driver = new FirefoxDriver();
+			driver.manage().window().maximize();
+			performAction();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public void performAction()
+	{
+		
+	}
+	
+	
+	public void closeBrowser()
+	{
+		
+	}*/
+	
+	
+	public static void main(String[]args)
+	{
+		System.out.println("start of java program");
+		idfcbankIRTC start=new idfcbankIRTC();
+		// start.invokeBrowser();
+		
+		
+	}
+
+}
