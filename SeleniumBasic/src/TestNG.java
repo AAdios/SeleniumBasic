5d780b2fb45a8257e54c877103207c4e4e8bc492
AAdios <varma096@gmail.com>
2018-12-26 11:35:38 +0530

    First gmailLogin Script

--- SeleniumBasic/src/TestNG.java
+++ SeleniumBasic/src/TestNG.java
@@ -1,4 +1,5 @@
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.*;
@@ -7,15 +8,30 @@ public class TestNG {
 	WebDriver driver; 
 	
 	
-@Test
+@BeforeTest
 public void testA() throws InterruptedException
 {
 	System.setProperty("webdriver.gecko.driver", "C:\\Users\\geckodriver.exe");
 	 driver=new FirefoxDriver();
 	 driver.manage().window().maximize();
-	 Thread.sleep(20000);
-	 driver.get("https://www.google.com/");
+	 Thread.sleep(10000);
+	 driver.get("https://accounts.google.com/");
 	 
+}
+@Test
+public void loginTest() throws InterruptedException
+{
+	driver.findElement(By.id("identifierId")).sendKeys("varma096@gmail.com");
+	driver.findElement(By.xpath("/html/body/div[1]/div[1]/div[2]/div[2]/div/div/div[2]/div/div[2]/div/div[1]/div/content/span")).click();
+	Thread.sleep(10000);
+	driver.findElement(By.xpath("/html/body/div[1]/div[1]/div[2]/div[2]/div/div/div[2]/div/div[1]/div/form/content/section/div/content/div[1]/div/div[1]/div/div[1]/input")).sendKeys("adesh@4655");
+	driver.findElement(By.xpath("/html/body/div[1]/div[1]/div[2]/div[2]/div/div/div[2]/div/div[2]/div/div[1]/div/content/span")).click();
+	
+}
+@AfterTest
+public void closeBrowser() throws InterruptedException
+{Thread.sleep(30000);
+	driver.close();
 }
 
 
